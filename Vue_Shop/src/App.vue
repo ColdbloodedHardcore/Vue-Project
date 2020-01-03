5fd469ee2821df1ee2aa2c9893d8740013e2542f
Popcoren <chaoticmathcore@gmail.com>
2020-01-03 15:48:13 +0200

    add sass

--- Vue_Shop/src/App.vue
+++ Vue_Shop/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="shop">
-    <div class="container-fluid">
+    <div class="container">
       <site-header/>   
       <post :posts="posts"
       />
@@ -16,60 +16,61 @@
     name: 'shop',
     data() {
       return {
+        state: 'home',
         posts: [
           {
             src: require ('./assets/images/Bitter Taste - Leave Me Broken (EP) (2019).jpg'),
             title: 'Bitter Taste - Leave Me Broken (EP) (2019)',
-            description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi sed beatae molestiae, illo assumenda, illum optio omnis iste voluptas amet, dolorum dicta. Hic maiores perferendis quas, voluptates quo ratione omnis!',
+            description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi sed beatae molestiae, illo assumenda, illum optio omnis iste voluptas amet, dolorum dicta!',
             genre: 'Metallic Hardcore',
             price: 14.99
           },
           {
             src: require ('./assets/images/Brothers Till We Die - The Thin Line Between Death And Immortality (2017).jpg'),
             title: 'Brothers Till We Die - Line Between Death And Immortality (2017)',
-            description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi sed beatae molestiae, illo assumenda, illum optio omnis iste voluptas amet, dolorum dicta. Hic maiores perferendis quas, voluptates quo ratione omnis!',
+            description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi sed beatae molestiae, illo assumenda, illum optio omnis iste voluptas amet, dolorum dicta!',
             genre: 'Hardcore',
             price: 14.99
           },
           {
             src: require ('./assets/images/Dead Kennedys - Give Me Convenience or Give Me Death (Remastered) (2001).jpg'),
             title: 'Dead Kennedys - Convenience or Give Me Death (2001)',
-            description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi sed beatae molestiae, illo assumenda, illum optio omnis iste voluptas amet, dolorum dicta. Hic maiores perferendis quas, voluptates quo ratione omnis!',
+            description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi sed beatae molestiae, illo assumenda, illum optio omnis iste voluptas amet, dolorum dicta!',
             genre: 'Hardcore Punk',
             price: 14.99
           },
           {
             src: require ('./assets/images/Hollowed Out - Life Among the Lifeless (2017).jpg'),
             title: 'Hollowed Out - Life Among the Lifeless (2017)',
-            description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi sed beatae molestiae, illo assumenda, illum optio omnis iste voluptas amet, dolorum dicta. Hic maiores perferendis quas, voluptates quo ratione omnis!',
+            description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi sed beatae molestiae, illo assumenda, illum optio omnis iste voluptas amet, dolorum dicta!',
             genre: 'Beatdown Hardcore',
             price: 14.99
           },
           {
             src: require ('./assets/images/Misantropic - Insomnia (2010).jpg'),
             title: 'Misantropic - Insomnia (2010)',
-            description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi sed beatae molestiae, illo assumenda, illum optio omnis iste voluptas amet, dolorum dicta. Hic maiores perferendis quas, voluptates quo ratione omnis!',
+            description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi sed beatae molestiae, illo assumenda, illum optio omnis iste voluptas amet, dolorum dicta!',
             genre: 'Crust, D Beat',
             price: 14.99
           },
           {
             src: require ('./assets/images/Spawn Of Disgust -  No Warning Shots (EP) (2016).jpg'),
             title: 'Spawn Of Disgust -  No Warning Shots (EP) (2016)',
-            description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi sed beatae molestiae, illo assumenda, illum optio omnis iste voluptas amet, dolorum dicta. Hic maiores perferendis quas, voluptates quo ratione omnis!',
+            description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi sed beatae molestiae, illo assumenda, illum optio omnis iste voluptas amet, dolorum dicta!',
             genre: 'Beatdown Hardcore',
             price: 14.99
           },
           {
             src: require ('./assets/images/Brothers Till We Die - We Will Never Grow Up (2014).jpg'),
             title: 'Brothers Till We Die - We Will Never Grow Up (2014)',
-            description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi sed beatae molestiae, illo assumenda, illum optio omnis iste voluptas amet, dolorum dicta. Hic maiores perferendis quas, voluptates quo ratione omnis!',
+            description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi sed beatae molestiae, illo assumenda, illum optio omnis iste voluptas amet, dolorum dicta!',
             genre: 'Hardcore',
             price: 14.99
           },
           {
             src: require ('./assets/images/The Warriors - Monomyth (2019).jpg'),
             title: 'The Warriors - Monomyth (2019)',
-            description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi sed beatae molestiae, illo assumenda, illum optio omnis iste voluptas amet, dolorum dicta. Hic maiores perferendis quas, voluptates quo ratione omnis!',
+            description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi sed beatae molestiae, illo assumenda, illum optio omnis iste voluptas amet, dolorum dicta!',
             genre: 'Hardcore',
             price: 0
           }
--- Vue_Shop/src/assets/templates/Post.vue
+++ Vue_Shop/src/assets/templates/Post.vue
@@ -4,15 +4,17 @@
             v-for="post in posts">
 
             <div class="card">
-                <img :src="post.src" class="card-img-top" alt="">
+                <img :src="post.src" class="card-img-top" alt="1">
 
                 <div class="card-body">
                     <h5 class="card-title" :title="post.title">{{ post.title }}</h5>
                     <p class="card-text">{{ post.description }}</p>
                     <span class="text-success">{{ post.genre }}</span>
-                    <span class="text-danger">{{ post.price }}$</span>
 
-                    <button class="btn btn-primary text-right">Buy Album</button>
+                    <div class="card-price">
+                        <span class="text-danger">{{ post.price }}$</span>
+                        <button class="btn btn-dark">Add To Cart</button>
+                    </div>                   
                 </div>
             </div>
         </div>
@@ -24,22 +26,39 @@
         props: ['posts'],
         data () {
             return {
-            
+                
             }
         }
     }
 </script>
 
-<style>
-    .card {
-        height: 700px;
-    }
-
-    .card-title {
-        height: 50px;
-    }
-
+<style scoped lang="scss">    
     .post {
-        padding: 15px;
-    }
+        padding: 15px;   
+
+        .card {
+        box-shadow:0px 5px 8px 0px rgba(0,0,0,0.5);
+        -webkit-box-shadow:0px 5px 8px 0px rgba(0,0,0,0.5);
+        -moz-box-shadow:0px 5px 8px 0px rgba(0,0,0,0.5);
+
+            &-title {
+            height: 55px;
+            font-size: 18px;
+            }
+
+            &-text {
+            font-size: 14px;
+            }    
+
+            &-price {
+            display: flex;
+            justify-content: space-between;
+            margin-top: 15px;
+            
+                .btn {
+                    border-radius: 4px;
+                } 
+            }
+        }     
+    } 
 </style>
