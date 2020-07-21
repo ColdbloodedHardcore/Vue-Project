bba3ced44cb29f1165156327115cde174f14f8bc
Popcoren <chaoticmathcore@gmail.com>
2020-07-21 15:16:50 +0300

    First Nuxt Website

---
+++ Nuxt/pages/About.vue
@@ -0,0 +1,32 @@
+<template>
+  <section>
+      <h1>About</h1>
+
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi provident illo a, beatae nisi earum labore. Quisquam est nihil pariatur, explicabo consequatur commodi sapiente facilis maiores inventore placeat voluptatum mollitia?
+      Quaerat eaque, nostrum exercitationem pariatur suscipit quo quisquam mollitia omnis dolore incidunt expedita ex deleniti in ut, necessitatibus accusantium iure possimus sed consequatur doloribus atque! Aliquid porro nemo minima ut.
+      Reprehenderit dolorem error asperiores deleniti quisquam. Debitis, reiciendis recusandae rem quo sunt iure est explicabo molestiae eum error aperiam velit cum ipsum architecto voluptatem illo consectetur accusantium incidunt culpa praesentium!</p>
+      
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi provident illo a, beatae nisi earum labore. Quisquam est nihil pariatur, explicabo consequatur commodi sapiente facilis maiores inventore placeat voluptatum mollitia?
+      Quaerat eaque, nostrum exercitationem pariatur suscipit quo quisquam mollitia omnis dolore incidunt expedita ex deleniti in ut, necessitatibus accusantium iure possimus sed consequatur doloribus atque! Aliquid porro nemo minima ut.
+      Reprehenderit dolorem error asperiores deleniti quisquam. Debitis, reiciendis recusandae rem quo sunt iure est explicabo molestiae eum error aperiam velit cum ipsum architecto voluptatem illo consectetur accusantium incidunt culpa praesentium!</p>
+      
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi provident illo a, beatae nisi earum labore. Quisquam est nihil pariatur, explicabo consequatur commodi sapiente facilis maiores inventore placeat voluptatum mollitia?
+      Quaerat eaque, nostrum exercitationem pariatur suscipit quo quisquam mollitia omnis dolore incidunt expedita ex deleniti in ut, necessitatibus accusantium iure possimus sed consequatur doloribus atque! Aliquid porro nemo minima ut.
+      Reprehenderit dolorem error asperiores deleniti quisquam. Debitis, reiciendis recusandae rem quo sunt iure est explicabo molestiae eum error aperiam velit cum ipsum architecto voluptatem illo consectetur accusantium incidunt culpa praesentium!</p>
+      
+  </section>
+</template>
+
+<script>
+import Navbar from  '@/components/Navbar'
+export default {
+  components: {
+    Navbar
+  }
+}
+</script>
+
+<style>
+
+</style>
+
---
+++ Nuxt/pages/Login.vue
@@ -0,0 +1,35 @@
+<template>
+    <section>      
+        <form action="#">
+            <h1>Login Page</h1>
+
+            <div class="form-group">
+                <input type="text" class="form-control">
+            </div>
+
+            <p>
+                <nuxt-link to="/">To Home Page</nuxt-link>
+            </p>
+
+            <button class="btn btn-primary" type="submit">Login</button>
+        </form>
+    </section>  
+</template>
+
+<script>
+export default {
+    layout: 'empty'
+}
+</script>
+
+<style scoped>
+    h1 {
+        padding-bottom: 1rem;
+    }
+
+    form {
+        width: 600px;
+        margin: 0 auto;
+    }
+</style>
+
